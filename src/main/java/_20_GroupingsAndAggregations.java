7a67a98727e260ce6ab90a91f3658a1fc650cad0
Stanislav Pekarovič <spekarovic@gmail.com>
2020-01-02 12:04:59 +0100

    Section 20: Groupings And Aggregations

---
+++ src/main/java/_20_GroupingsAndAggregations.java
@@ -0,0 +1,52 @@
+import joptsimple.internal.Rows;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _20_GroupingsAndAggregations {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder()
+                .appName("testingSql")
+                .master("local[*]")
+                .config("spark.sql.warehouse.dir", "file:///c:/tmp/")
+                .getOrCreate();
+
+        List<Row> inMemory = new ArrayList<Row>();
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
+
+        StructField[] fields = new StructField[] {
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType schema = new StructType(fields);
+        Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+
+        dataset.createOrReplaceTempView("logs");
+
+        Dataset<Row> results = spark.sql("select level, count(1) as count from logs group by level order by count desc");
+
+        results.show();
+
+        spark.close();
+    }
+
+}
